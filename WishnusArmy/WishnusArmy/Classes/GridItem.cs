31a75d7d34129ed11ec5f61967f0147e747e6fca
Hugo Peters <hpeters1024@gmail.com>
2016-12-01 17:38:53 +0100

    Added GridLevel and GridItem class and implemented a simple texture.

---
+++ WishnusArmy/WishnusArmy/Classes/GridItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+public class GridItem
+{
+    int obj; //Indicator for what is placed on the square (0 for emtpy)
+    int texture; //Indicator for what texture is placed on the square
+    Texture2D tex;
+
+    public GridItem(ContentManager Content, int texture = 0, int obj = 0)
+    {
+        this.texture = texture;
+        this.obj = obj;
+        tex = Content.Load<Texture2D>("Content/Textures/tex_grass");
+    }
+
+    public void Draw(GameTime gameTime, SpriteBatch spriteBatch, Vector2 pos)
+    {
+        spriteBatch.Draw(tex, pos, Color.White);
+    }
+}
+
---
+++ WishnusArmy/WishnusArmy/Classes/GridLevel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+public class GridLevel : IGameLoopObject
+{
+    const int LEVEL_SIZE = 10;
+    const int GRID_SIZE = 128;
+    GridItem[,] grid;
+        
+    public GridLevel(ContentManager Content)
+    {
+        //Initialize the grid with the size of the level
+        grid = new GridItem[LEVEL_SIZE, LEVEL_SIZE]; 
+        //Fill the grid with GridItems
+        for(int x=0; x<LEVEL_SIZE; ++x)
+        {
+            for(int y=0; y<LEVEL_SIZE; ++y)
+            {
+                grid[x, y] = new GridItem(Content);
+            }
+        }
+    }
+
+    public virtual void HandleInput(InputHelper inputHelper)
+    {
+
+    }
+
+    public virtual void Update(GameTime gameTime)
+    {
+
+    }
+
+    public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+    {
+        //Execute the draw event for every GridItem in the grid
+        for(int x=0; x<LEVEL_SIZE; ++x)
+        {
+            for(int y=0; y<LEVEL_SIZE; ++y)
+            {
+                grid[x, y].Draw(gameTime, spriteBatch, new Vector2(GRID_SIZE*x, GRID_SIZE*y));
+            }
+        }
+    }
+
+    public virtual void Reset()
+    {
+
+    }
+}
