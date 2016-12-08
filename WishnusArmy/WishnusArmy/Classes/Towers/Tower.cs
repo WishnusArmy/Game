5b278d865165e565a2c2179666fbeb2c0131ecf8
Wouter Castelein <wmcastelein@gmail.com>
2016-12-08 12:07:59 +0100

    Tower classes

---
+++ WishnusArmy/WishnusArmy/Classes/Towers/Tower.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Constant;
+
+namespace WishnusArmy.Classes.Towers
+{
+    class Tower : AnimatedGameObject
+    {
+        Vector2 gridPosition;
+        public Texture2D texture;
+        int range;
+        int level = 1;
+        int cost;
+
+        public Tower(Vector2 position)
+
+        {
+            gridPosition = position;
+        }
+        
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            Vector2 pos = position *= NODE_SIZE;
+            spriteBatch.Draw(texture, pos);
+        }
+
+        public virtual void findNearestEnemy()
+        {
+
+        }
+
+        public virtual void Attack()
+        {
+
+        }
+    }
+}
