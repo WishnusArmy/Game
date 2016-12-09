47e193db6b5291dd1089d253e447c586e1ca230a
Wouter Castelein <wmcastelein@gmail.com>
2016-12-09 11:02:37 +0100

    Towers now only target positions within a certain range

--- WishnusArmy/WishnusArmy/Classes/Towers/Tower.cs
+++ WishnusArmy/WishnusArmy/Classes/Towers/Tower.cs
@@ -11,18 +11,18 @@ using static ContentImporter;
 public class Tower : GameObject
 {
     protected MouseState state = new MouseState();
-    public Vector2 gridPosition, pos, mousePosition;
+    public Vector2 gridPosition, pos, mousePosition, previousPosition= new Vector2(0,0);
     public Texture2D baseTexture;
     public Texture2D cannonTexture;
     float rotation;
-    int range;
+    int range = 5 * Constant.NODE_SIZE;
     int level = 1;
     int cost;
 
     public Tower()
     {
-        baseTexture = Sprites.SPR_ABSTRACT_TOWER;
-        cannonTexture = Sprites.SPR_ABSTRACT_CANNON;
+        baseTexture = Sprites.SPR_ABSTRACT_TOWER; //Texture of the (unanimated) base of the tower
+        cannonTexture = Sprites.SPR_ABSTRACT_CANNON; // The moving part of a tower
     }
         
     public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
@@ -34,15 +34,30 @@ public class Tower : GameObject
     }
     public override void HandleInput(InputHelper inputHelper)
     {
-        double opposite = inputHelper.MousePosition.Y -cannonTexture.Width/2 - pos.Y;
-        double adjacent = inputHelper.MousePosition.X -cannonTexture.Width/2 - pos.X;
+        //calculate the rotation of the cannonbarrel
+        double opposite = findTarget().Y -cannonTexture.Width/2 - pos.Y;
+        double adjacent = findTarget().X -cannonTexture.Width/2 - pos.X;
         rotation = (float)Math.Atan2(opposite, adjacent) + 0.5f * (float) Math.PI;
+        
         mousePosition = inputHelper.MousePosition;
     }
-    public Vector2 findNearestEnemy()
+    public virtual Vector2 findTarget()
     {
-        return mousePosition;
-        //Change this into closest enemy position
+        //Calculate the distance of the target
+        double A = mousePosition.Y - pos.Y;
+        double B = mousePosition.X - pos.X;
+        double distance = Math.Sqrt(A*A + B*B);
+
+        // if target is withing the tower range
+        if (distance <= range)
+        {
+            previousPosition = mousePosition;
+            return mousePosition;
+            //Change this into closest enemy position
+        }
+        else
+            return previousPosition;
+        
     }
 
     public virtual void Attack()
