6a1d2d966380c00b085ddbdb5ff8ed33e1c8c7d6
Hugo Peters <hpeters1024@gmail.com>
2017-01-09 00:16:01 +0100

    Maurin je hebt alles verneukt

---
+++ WishnusArmy/WishnusArmy/Classes/Towers/Abstract/CannonTower.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+using static Constant;
+using static ContentImporter.Sprites;
+
+public abstract class CannonTower : Tower
+{
+    float rotation;
+    protected Vector2 targetPos;
+    protected Texture2D cannonTexture;
+
+
+    public CannonTower() : base()
+    {
+        cannonTexture = SPR_ABSTRACT_CANNON; // The moving part of a tower
+    }
+    public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+    {
+        base.Draw(gameTime, spriteBatch);
+        spriteBatch.Draw(cannonTexture, GlobalPosition, null, null, new Vector2(cannonTexture.Width / 2, cannonTexture.Height / 2), rotation);
+    }
+
+    public override void HandleInput(InputHelper inputHelper)
+    {
+        base.HandleInput(inputHelper);
+        //calculate the rotation of the cannonbarrel
+        if (target != null)
+        {
+            targetPos = target.Position;
+        }
+        double opposite = targetPos.Y - GlobalPosition.Y;
+        double adjacent = targetPos.X - GlobalPosition.X;
+        rotation = (float)Math.Atan2(opposite, adjacent) + 0.5f * (float)Math.PI;
+    }
+}
--- WishnusArmy/WishnusArmy/Classes/Towers/Base.cs
+++ WishnusArmy/WishnusArmy/Classes/Towers/Base.cs
@@ -9,7 +9,7 @@ using Microsoft.Xna.Framework.Graphics;
 using static ContentImporter.Sprites;
 using static Constant;
 
-public class Base : Tower
+public class Base : CannonTower
 {
     Vector2 GameMiddle = new Vector2(LEVEL_SIZE.X * NODE_SIZE.X / 2, LEVEL_SIZE.Y * NODE_SIZE.Y / 4);
     Vector2 BaseOrigin = new Vector2(SPR_BASEGUN.Width / 2, SPR_BASEGUN.Height / 2);
@@ -18,15 +18,15 @@ public class Base : Tower
     {
         //this.gridPosition = new Vector2(LEVEL_SIZE / 2, LEVEL_SIZE / 4) - BaseOrigin/NODE_SIZE.X;
         //this.gridPosition = new Vector2(5,5);
-        //this.cannonTexture = SPR_BASEGUN;
+        this.cannonTexture = SPR_BASEGUN;
         sprite = SPR_BASE;
         //this.range = 0; //(aimed manually)
     }
 
     public override void HandleInput(InputHelper inputHelper)
     {
-        //base.HandleInput(inputHelper);
-        //targetPos = inputHelper.MousePosition;
+        base.HandleInput(inputHelper);
+        targetPos = inputHelper.MousePosition;
     }
 
     public override void Attack()
