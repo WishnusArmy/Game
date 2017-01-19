e683a51af522262a84ebea9cd6201510b8a9ee99
Abel van Beek <a.vanbeek@students.uu.nl>
2017-01-19 15:28:19 +0100

    Added helicopter and shadow + random fixes

--- WishnusArmy/WishnusArmy/Classes/Enemies/Abstract/EnemySpawner.cs
+++ WishnusArmy/WishnusArmy/Classes/Enemies/Abstract/EnemySpawner.cs
@@ -50,7 +50,7 @@ public class EnemySpawner : GameObjectList
             {
                 resources -= 100;
                 GridNode node = available[RANDOM.Next(available.Count)];
-                Add(new Tank() { startNode = node, Position = node.Position });
+                Add(new Airplane() { startNode = node, Position = node.Position });
             }
 
             bool AllDeath = true;
---
+++ WishnusArmy/WishnusArmy/Classes/Enemies/Air/Helicopter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using static ContentImporter.Sheets;
+using static Constant;
+
+public class Helicopter : EnemyAir
+{
+    public Helicopter() : base(Type.Helicopter, SHEET_HELICOPTER)
+    {
+        this.sprite = SHEET_HELICOPTER;
+        this.speed = 2;
+    }
+}
