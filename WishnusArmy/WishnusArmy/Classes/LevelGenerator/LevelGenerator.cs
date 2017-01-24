0af91d1537646e35135992bb53dede2bba7d1eda
Wouterbaas8 <wubbink@xs4all.nl>
2017-01-24 12:52:36 +0100

    ALLAHU AKBAR

--- WishnusArmy/WishnusArmy/Classes/LevelGenerator/LevelGenerator.cs
+++ WishnusArmy/WishnusArmy/Classes/LevelGenerator/LevelGenerator.cs
@@ -24,7 +24,7 @@ public class LevelGenerator : GameObject
     public List<int[,]> GenerateNewLevel()
     {
         ClearGrid();
-        GenerateSpecialTiles(5, 43, 5);     //Populate the land level with forests
+        GenerateSpecialTiles(5, 45, 5);     //Populate the land level with forests
         GenerateSpecialTiles(2, 43, 5);     //Populate the land level with mountains
         GenerateSpecialTiles(4, 40, 5);     //Populate the land level with rivers
 
